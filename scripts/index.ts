f1c697d2708ad6528578f831dcbe38bd9401dbbd
Cyrille <cyrille.rieau@gmail.com>
2017-10-10 12:30:31 +0200

    Observable

--- scripts/index.ts
+++ scripts/index.ts
@@ -11,3 +11,10 @@ observer.next(3);
 observer.complete();
 });
 blank$.subscribe((val)=>console.log(val));
+
+const button = document.querySelector('button');
+const click$ = Observable.fromEvent(button, 'click');
+click$.subscribe((event)=> console.log('click'));
+
+const tableau$ = Observable.from(['ga', 'zo', 'bu', 'meu']).filter((item)=>item.length <= 2);
+tableau$.subscribe((item)=> console.log(item));
