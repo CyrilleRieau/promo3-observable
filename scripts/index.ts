2c7d4329601d59865207cba933644ebc67249f79
Cyrille <cyrille.rieau@gmail.com>
2017-10-10 17:27:32 +0200

    Test Tableau et input

--- scripts/index.ts
+++ scripts/index.ts
@@ -23,22 +23,37 @@ const tableau$ = Observable.from(['ga', 'zo', 'bu', 'meu']);
 //.find((item)=>item==='ga');
 tableau$.subscribe((item)=> console.log(item));
 
-const input = document.querySelector('input');
+//const input = document.querySelector('input');
 // const inputext$ = Observable.fromEvent(input, 'keyup')
 // .map((event) => event.target.value)
 // .map((value) => value.toUpperCase())
 // .debounceTime(300);
 // inputext$.subscribe((value) => console.log(value));
 
-const keypress$ = Observable.fromEvent(input, 'keyup')
-.map((event:KeyboardEvent) => event.keyCode)
-.bufferTime(3000);
-const combo = [37, 39, 38, 40, 65, 65, 66, 66];
-keypress$.subscribe(
-//  (value)=>console.log(value);
-(value) => {if ((value.length === combo.length) && (value.every((element, index)=> element === combo[index]))) {
-    alert('Bravo');
-} else {
-     console.log('Fail');
- }
-});
+// const keypress$ = Observable.fromEvent(input, 'keyup')
+// .map((event:KeyboardEvent) => event.keyCode)
+// .bufferTime(3000);
+// const combo = [37, 39, 38, 40, 65, 65, 66, 66];
+// keypress$.subscribe(
+// //  (value)=>console.log(value);
+// (value) => {if ((value.length === combo.length) && (value.every((element, index)=> element === combo[index]))) {
+//     alert('Bravo');
+// } else {
+//      console.log('Fail');
+//  }
+// });
+
+const inputs = document.querySelectorAll('input');
+
+const nom$= Observable.fromEvent(inputs[0], 'keyup').map((event:any)=> event.target.value).filter(value => value !== '');
+const prenom$= Observable.fromEvent(inputs[1], 'keyup').map((event:any)=> event.target.value).filter(value => value !== '');
+const age$= Observable.fromEvent(inputs[2], 'keyup').map((event:any)=> event.target.value).filter(value => value !== '');
+
+const personne$ = Observable.combineLatest(nom$, prenom$, age$).map((values)=>{
+    return {
+        nom : values[0],
+        prenom: values[1],
+        age : values[2]
+    }
+}).debounceTime(300);
+personne$.subscribe((personne)=>console.log(personne));
